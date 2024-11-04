790607fb6cff37ed5b3ec1474cc809fa1eb4526c
Enzo Tinard <121262955+enzxt@users.noreply.github.com>
2024-11-03 20:13:14 -0700

    s

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-// Start the server on the Heroku-assigned port
+// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
