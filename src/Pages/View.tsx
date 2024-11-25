7ef9615037c8fa13b792a32fee540772d8e46ff2
HudsonHughes <hhughes26@my.gcu.edu>
2024-11-24 21:33:06 -0700

    revert

--- src/Pages/View.tsx
+++ src/Pages/View.tsx
@@ -23,7 +23,7 @@ const View: React.FC = () => {
             <th>Actions</th>
           </tr>
         </thead>
-        <tbody style={{ backgroundColor: "#0000FF" }}>
+        <tbody style={{ backgroundColor: "#f9f9f9" }}>
           {cars.map((car) => (
             <tr
               key={car.id}
